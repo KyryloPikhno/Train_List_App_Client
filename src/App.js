4ea25754f74097e968b52abdedc444b8be6fafac
KyryloPikhno <Mr.Good@i.ua>
2023-04-22 03:07:23 +0300

    added redux

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import {Navigate, Route, Routes} from "react-router-dom";
+
+import {Layout} from "./layouts";
+import {TrainsPage} from "./pages";
+
+
 const App = () => {
 
   return (
-      <div>
-        app
-      </div>
+      <Routes>
+          <Route path={'/'} element={<Layout/>}>
+              <Route index element={<Navigate to={'/trains-list'}/>}/>
+              <Route path={'/trains-list'} element={<TrainsPage/>}/>
+          </Route>
+      </Routes>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import {BrowserRouter} from "react-router-dom";
 import ReactDOM from 'react-dom/client';
+import {Provider} from "react-redux";
+import React from 'react';
 
 import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <BrowserRouter>
+        {/*<Provider>*/}
+            <App/>
+        {/*</Provider>*/}
+    </BrowserRouter>
 );
+
 
